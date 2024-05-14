94ec84cb695829967522d8d61ec82dbec08de31c
rybernate <rybernate@gmail.com>
2024-05-14 00:47:44 -0700

    completed project

---
+++ freeCodeCamp Roman Numeral Converter/script.js
@@ -0,0 +1,88 @@
+// JS for rybernate's Solution to Build a Roman Numeral Converter (freeCodeCamp)
+const numberInput = document.getElementById("number");
+const numberOutput = document.getElementById("output");
+const convertBtn = document.getElementById("convert-btn");
+let convertedNum;
+
+const conversion = (value) => {
+  const arabicNum = [
+    1000,
+    900,
+    500,
+    400,
+    100,
+    90,
+    50,
+    40,
+    10,
+    9,
+    5,
+    4,
+    1
+  ];
+  const romanNum = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ];
+
+  convertedNum = "";
+  for(let i = 0; value; i++){
+    while(value >= arabicNum[i]){
+      value -= arabicNum[i];
+      convertedNum += romanNum[i];
+    }
+  }
+  return;
+};
+
+const convertNumber = () => {
+  const number = numberInput.value;
+  if (number === "") {
+
+    numberOutput.style.fontSize = "1.4rem";
+    numberOutput.style.display = "block";
+    numberOutput.innerText = "Please enter a valid number"; 
+
+  }
+  else if (number <= 0) {
+
+    numberOutput.style.fontSize = "1.4rem";
+    numberOutput.style.display = "block";
+    numberOutput.innerText = "Please enter a number greater than or equal to 1";
+
+  }  
+  else if (number >= 4000) {
+
+    numberOutput.style.fontSize = "1.4rem";
+    numberOutput.style.display = "block";
+    numberOutput.innerText = "Please enter a number less than or equal to 3999";
+
+  } 
+  else {
+
+    conversion(number);
+    numberOutput.style.display = "block";
+    numberOutput.style.fontSize = "3rem";
+    numberOutput.innerText = convertedNum;
+
+  };
+};
+
+convertBtn.addEventListener("click", convertNumber);
+numberInput.addEventListener("keydown", e => {
+  if (e.key === "Enter") {
+    convertNumber();
+
+  }
+});
